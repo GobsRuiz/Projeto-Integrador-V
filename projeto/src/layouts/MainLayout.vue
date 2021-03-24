1b615f6d7fff546da57be81ab450de4fd28ddf19
Gabriel Ruiz <51479498+GobsRuiz@users.noreply.github.com>
2021-03-24 11:57:35 -0300

    Upando
    A logo foi inserida no cabeçalho.
    A primeira imagem do carrossel foi inserida.
    A seção divulgação está finalizada
    O rodapé está finalizado.
    Falta inserir algumas imagens do carrosel, da seção organização e da seção campanha.

--- projeto/src/layouts/MainLayout.vue
+++ projeto/src/layouts/MainLayout.vue
@@ -3,14 +3,14 @@
     <!-- Header -->
     <q-header elevated>
       <!-- Social networking -->
-      <q-toolbar class="justify-center">
+      <q-toolbar id="toolbar1" class="justify-center">
         <q-btn to="/sobre" icon-right="facebook" label="Nos siga no facebook" flat rounded />
         <q-btn to="/sobre" icon-right="instagram" label="E no instagram" flat rounded />
         <q-btn to="/sobre" icon-right="mail" label="Ou nos envie um e-mail" flat rounded />
       </q-toolbar>
 
       <!-- Logo and menu -->
-      <q-toolbar>
+      <q-toolbar id="toolbar2" class="q-py-lg text-black">
         <q-btn
           flat
           dense
@@ -21,16 +21,16 @@
           class="desktop-hide"
         />
 
-        <q-toolbar-title>
-          Quasar App
+        <q-toolbar-title id="toolbar2-logo">
+          <img src="../assets/logo.svg" alt="">
         </q-toolbar-title>
 
         <div class="mobile-hide">
-          <q-btn to="/" label="Início" flat rounded></q-btn>
-          <q-btn to="/sobre" label="Organizações" flat rounded></q-btn>
-          <q-btn to="/sobre" label="Campanhas" flat rounded></q-btn>
-          <q-btn to="/sobre" label="Contato" flat rounded></q-btn>
-          <q-btn to="/sobre" label="Sobre" flat rounded></q-btn>
+          <q-btn to="/" label="Início" flat rounded class="text-h5"></q-btn>
+          <q-btn to="/sobre" label="Organizações" flat rounded class="text-h5"></q-btn>
+          <q-btn to="/sobre" label="Campanhas" flat rounded class="text-h5"></q-btn>
+          <q-btn to="/sobre" label="Contato" flat rounded class="text-h5"></q-btn>
+          <q-btn to="/sobre" label="Sobre" flat rounded class="text-h5"></q-btn>
         </div>
       </q-toolbar>
     </q-header>
@@ -60,6 +60,49 @@
     <q-page-container>
       <router-view />
     </q-page-container>
+
+    <!-- Footer -->
+    <q-footer id="footer">
+      <!-- Logo -->
+      <div id="footer-logo" class="text-center q-pb-sm q-pt-md">
+        <img src="../assets/logo.svg" alt="">
+      </div>
+
+      <!-- Content -->
+      <div id="footer-content" class="flex items-center justify-between bg-primary q-pa-md">
+        <!-- Links || menu -->
+        <div class="column">
+          <q-btn to="/" label="Início" flat rounded class="text-h6"></q-btn>
+          <q-btn to="/sobre" label="Organizações" flat rounded class="text-h6"></q-btn>
+          <q-btn to="/sobre" label="Campanhas" flat rounded class="text-h6"></q-btn>
+          <q-btn to="/sobre" label="Contato" flat rounded class="text-h6"></q-btn>
+          <q-btn to="/sobre" label="Sobre" flat rounded class="text-h6"></q-btn>
+        </div>  
+        
+        <!-- Social networks -->
+        <div class="row">
+          <div class="column items-center">
+            <q-btn to="/sobre" label="Nos siga no facebook" flat rounded class="text-h6" />
+            <q-icon name="facebook" class="text-h4" />
+          </div>
+          <div class="column items-center">
+            <q-btn to="/sobre" label="E no instagram" flat rounded class="text-h6"/>
+            <q-icon name="instagram" class="text-h4" />
+          </div>
+          <div class="column items-center">
+            <q-btn to="/sobre" label="Ou nos envie um e-mail" flat rounded class="text-h6" />
+            <q-icon name="email" class="text-h4" />
+          </div>
+        </div>
+      </div>
+
+      <!-- Copyright -->
+      <div id="footer-copyright" class="q-py-lg">
+        <p class="text-black text-center text-h4" style="margin: 0;">
+          Centro de doações - 2021 - Todos os direitos reservados ©
+        </p>
+      </div>
+    </q-footer>
   </q-layout>
 </template>
 
@@ -122,3 +165,50 @@ export default {
   }
 }
 </script>
+
+<style>
+/* Toolbar1 */
+#toolbar1{
+  background-color: #00A5F7;
+}
+
+
+
+
+
+/* Toolbar2 */
+#toolbar2{
+  background-image: url("../assets/header/background-menu.svg");
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+
+/* Logo */
+#toolbar2-logo img{
+  width: 300px;
+}
+
+
+
+
+
+/* Footer */
+/* Logo */
+#footer-logo img{
+  width: 300px;
+}
+
+/* Content */
+#footer-content{
+  width: 100%;
+}
+
+/* Logo */
+/* Copyright */
+#footer-logo,
+#footer-copyright{
+  background-image: url("../assets/header/background-menu.svg");
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+</style>
--- projeto/src/pages/Index.vue
+++ projeto/src/pages/Index.vue
@@ -12,7 +12,7 @@
       @mouseleave="autoplay = true"
       height="500px"
     >
-      <q-carousel-slide class="flex justify-center items-center" name="first" img-src="https://cdn.quasar.dev/img/mountains.jpg">
+      <q-carousel-slide class="carousel-slide flex justify-center items-center" name="first" img-src="../assets/carousel/banner1.png">
         <div class="text-center" style="width: 30%">
           <div class="text-h2 text-white">First stop</div>
           <p class="text-subtitle1 text-white">
@@ -89,7 +89,7 @@
     <section class="q-pa-xl">
       <!-- Section title -->
       <h1 class="text-h4 text-center text-primary q-mt-none">
-        Instituições mais procuradas essa semana
+        Campanhas mais populares da semana
       </h1>
 
       <!-- Texts and image -->
@@ -129,14 +129,11 @@
       </div>
     </section>
 
-    <section class="bg-primary q-pa-xl">
-      <!-- Section title -->
-      <h1 class="text-h4">
-        Deseja ter as campanhas ou eventos beneficente divulgados no nosso site?
-      </h1>
-
-      <div class="flex justify-center items-center">
-        <q-btn color="white" class="text-black" label="clique aqui" />
+    <!-- Divulgation -->
+    <section id="divulgation" class="flex justify-center items-center q-py-xs">
+      <!-- Content -->
+      <div id="divulgation-content" class="text-center">
+        <q-btn color="white q-px-xl q-py-md" rounded class="text-black" label="clique aqui" size="22px" />
       </div>
     </section>
   </q-page>
@@ -153,3 +150,25 @@ export default {
   }
 }
 </script>
+<style>
+/* Carousel */
+/* Carousel */
+.carousel-slide{
+  background-size: 100% 100%;
+}
+
+
+
+/* Divulgation */
+#divulgation{
+  height: 100vh;
+
+  background-color: #00A5F7;
+  background-image: url("../assets/divulgation/banner.png");
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+#divulgation-content button{
+  transform: translateY(40px);
+}
+</style>
