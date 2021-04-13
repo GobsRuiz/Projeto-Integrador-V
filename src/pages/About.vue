a60c1e2100f51f9e3fc0fafbcdda8f27fdb373c0
jhoneffernandes <49643971+jhoneffernandes@users.noreply.github.com>
2021-04-13 16:56:38 -0300

    Página Sobre 1.0

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,11 +1,102 @@
 <template>
-  <q-page class="flex flex-center">
-    <h1>About</h1>
+  <q-page class="q-py-xl flex justify-center align-center">
+        <section title="O QUE É O CENTRO DE DOAÇÕES?" class="q-pt-xl" id="sectioncreation">
+        <div style="transform: scaleY(-1);">
+    <h1 id="organizations-title" class="text-h3 textColor-blue text-center q-mb-xl q-px-md">
+O QUE É O CENTRO DE DOAÇÕES?
+    </h1>
+    <div class="row justify-center q-py-xl">
+    <img src="../assets/about/faqabout.svg" alt="">
+</div>
+    <p class="text-center text-h5 text-weight-bolder">
+      O Centro de Doações é uma instituição voltada para facilitar a criação de campanhas
+e eventos voltados para arrecadação de fundos, o maior objetivo do CDD é poder
+dar mais visibilidade as campanhas, assim como também ser um local em que as pessoas
+possam ter mais informações sobre as instituições.
+    </p>
+    </div>
+    </section>
+
+        <section title="COMO O CENTRO DE DOAÇÕES FOI CRIADO?" class="q-py-xl" id="sectionstory">
+    <h1 id="organizations-title" class="text-h3 textColor-blue text-center q-mb-xl q-px-md">
+COMO O CENTRO DE DOAÇÕES FOI CRIADO?
+    </h1>
+        <div class="row justify-center q-py-xl">
+    <img src="../assets/about/readingabout.svg" alt="">
+</div>
+    <p class="text-center text-h5 text-weight-bolder">
+      O CDD foi criado em 2021, e a ideia veio dos estudantes do curso de Sistemas para Internet do 5º
+semestre na Fatec Taquaritinga, a ideia teve origem como uma necessidade de tema
+para o desenvolvimento do Projeto Integrador V, e então os estudantes do grupo Esquadrão Suicida
+em uma reunião chegaram a conclusão de criar uma instituição que funcionasse como
+centralizadora de doações, para dar mais visibilidade as campanhas e as organizações beneficentes
+    </p>
+    </section>
+
+            <section title="OS VALORES DO CENTRO DE DOAÇÕES" class="q-py-xl" id="sectioncreation">
+        <div style="transform: scaleY(-1);">
+    <h1 id="organizations-title" class="text-h3 textColor-blue text-center q-mb-xl q-px-md">
+OS VALORES DO CENTRO DE DOAÇÕES
+    </h1>
+        <div class="row justify-center q-py-xl">
+    <img src="../assets/about/meetingabout.svg" alt="">
+</div>
+         <div class="row text-center">
+
+           <div class="column col-4 items-center">
+             <h3 class="text-h5 text-weight-bolder">ATENCIOSIDADE</h3>
+           <q-icon name="far fa-eye text-light-blue-6" class="text-h1 q-pb-xl" />
+          <p class="text-h6 text-weight-bold">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi, tempora expedita vero sint nemo corporis rerum alias. Doloribus voluptatem, debitis adipisci commodi, ipsam, labore reprehenderit distinctio reiciendis deserunt beatae facere?
+          Eum ut, nesciunt rem magni atque est, voluptatum, sequi possimus deserunt doloribus quis? Esse a vero rerum, atque unde impedit enim veritatis distinctio optio, ab modi tempore quas at quam!
+          Eum aspernatur exercitationem, commodi hic quibusdam autem similique culpa dolorem harum deleniti expedita, quia ratione illo fuga ut adipisci quas doloribus quidem praesentium! Ducimus in similique expedita? Beatae, dicta voluptatibus.
+          Illo porro ea harum omnis aspernatur quidem, cum delectus exercitationem quis libero beatae quia hic ipsa ab laborum minima suscipit corrupti. Vel laboriosam officiis velit exercitationem corporis sunt quos. Modi?</p>
+          </div>
+
+  <div class="column col-4 items-center">
+                 <h3 class="text-h5 text-weight-bolder">SIMPLICIDADE</h3>
+           <q-icon name="far fa-handshake text-light-blue-6" class="text-h1 q-pb-xl" />
+          <p class="text-h6 text-weight-bold">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi, tempora expedita vero sint nemo corporis rerum alias. Doloribus voluptatem, debitis adipisci commodi, ipsam, labore reprehenderit distinctio reiciendis deserunt beatae facere?
+          Eum ut, nesciunt rem magni atque est, voluptatum, sequi possimus deserunt doloribus quis? Esse a vero rerum, atque unde impedit enim veritatis distinctio optio, ab modi tempore quas at quam!
+          Eum aspernatur exercitationem, commodi hic quibusdam autem similique culpa dolorem harum deleniti expedita, quia ratione illo fuga ut adipisci quas doloribus quidem praesentium! Ducimus in similique expedita? Beatae, dicta voluptatibus.
+          Illo porro ea harum omnis aspernatur quidem, cum delectus exercitationem quis libero beatae quia hic ipsa ab laborum minima suscipit corrupti. Vel laboriosam officiis velit exercitationem corporis sunt quos. Modi?</p>
+          </div>
+
+            <div class="column col-4 items-center">
+                           <h3 class="text-h5 text-weight-bolder">PROFISSIONALISMO</h3>
+           <q-icon name="fas fa-id-card text-light-blue-6" class="text-h1 q-pb-xl" />
+          <p class="text-h6 text-weight-bold">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi, tempora expedita vero sint nemo corporis rerum alias. Doloribus voluptatem, debitis adipisci commodi, ipsam, labore reprehenderit distinctio reiciendis deserunt beatae facere?
+          Eum ut, nesciunt rem magni atque est, voluptatum, sequi possimus deserunt doloribus quis? Esse a vero rerum, atque unde impedit enim veritatis distinctio optio, ab modi tempore quas at quam!
+          Eum aspernatur exercitationem, commodi hic quibusdam autem similique culpa dolorem harum deleniti expedita, quia ratione illo fuga ut adipisci quas doloribus quidem praesentium! Ducimus in similique expedita? Beatae, dicta voluptatibus.
+          Illo porro ea harum omnis aspernatur quidem, cum delectus exercitationem quis libero beatae quia hic ipsa ab laborum minima suscipit corrupti. Vel laboriosam officiis velit exercitationem corporis sunt quos. Modi?</p>
+          </div>
+
+          </div>
+
+    </div>
+    </section>
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'PageIndex'
+  name: 'PageAbout'
 }
 </script>
+
+<style scoped>
+#sectionstory{
+  background-image: url("../assets/header/background-menu.svg");
+  background-repeat: no-repeat;
+  background-size: cover;}
+
+  #sectioncreation{
+  background-image: url("../assets/header/background-menu.svg");
+  background-repeat: no-repeat;
+  background-size: cover;
+    transform: scaleY(-1);
+  }
+
+  #sectioncreation img, #sectionstory img{
+  width: 350px;  
+  }
+</style>
